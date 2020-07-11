cdf23a7f58108587a107129d2f384b8e465939f1
Xerxos1 <timpolklaesener@web.de>
2020-07-11 05:02:15 +0200

    added adde flag prefab

---
+++ 3D Game Jam/Assets/Scripts/FlagTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class FlagTrigger : MonoBehaviour
+{
+    public Transform flag;
+    private bool triggered;
+    private Vector3 start;
+    private Vector3 target;
+    private float speed = 15f;
+    private void OnTriggerEnter()
+    {
+        target = new Vector3(flag.position.x, flag.position.y - 30, flag.position.z);
+        triggered = true;
+    }
+
+    void Update() 
+    { 
+        if(triggered)
+            flag.position = Vector3.MoveTowards(flag.position, target, Time.deltaTime * speed);
+    }
+
+}

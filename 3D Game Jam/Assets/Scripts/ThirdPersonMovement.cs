95e9debdd6464d0c8f27acff8d1c626bdbcc7806
Finn <zockeremail2@yahoo.de>
2020-07-11 22:46:32 +0200

    merged Tim2 to dev

--- 3D Game Jam/Assets/Scripts/ThirdPersonMovement.cs
+++ 3D Game Jam/Assets/Scripts/ThirdPersonMovement.cs
@@ -3,6 +3,7 @@
 public class ThirdPersonMovement : MonoBehaviour
 {
     public Transform Camera;
+    public Transform groundCheck;
     public CharacterController Controller;
 
     public float playerSpeed = 25f,
@@ -11,6 +12,7 @@ public class ThirdPersonMovement : MonoBehaviour
                     gravityMultiplier = 0.15f;
 
     private Vector3 playerVelocity = new Vector3(0, 0, 0);
+<<<<<<< HEAD
     private bool isGrounded, canJump;
 
     private void Start()
@@ -21,6 +23,25 @@ public class ThirdPersonMovement : MonoBehaviour
     private void FixedUpdate()
     {
         isGrounded = Controller.isGrounded;
+=======
+    private bool isGrounded = false;
+    private float playerSpeed = 20.0f;
+    private float jumpHeight = 1f;
+    private float gravityValue = -9.81f;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        Debug.Log(isGrounded);
+
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+>>>>>>> origin/Tim2
 
         RotateCharacterTowardsCameraAim();
 
@@ -33,6 +54,7 @@ public class ThirdPersonMovement : MonoBehaviour
 
     private void RotateCharacterTowardsCameraAim() => transform.rotation = Quaternion.Euler(0f, Camera.transform.rotation.eulerAngles.y, 0f);
 
+<<<<<<< HEAD
     private void MoveCharacterTowardsRotation() => Controller.Move(transform.forward * playerSpeed * Time.fixedDeltaTime);
 
     private void ExecuteIfJumped()
@@ -50,6 +72,9 @@ public class ThirdPersonMovement : MonoBehaviour
     private void AddGravityToCharacter()
     {
         if (isGrounded)
+=======
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+>>>>>>> origin/Tim2
         {
             playerVelocity = Vector3.zero;
             canJump = true;

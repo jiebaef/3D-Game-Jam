91f601d8d949dd912133c7f564db6b1a4a2daa60
Finn <zockeremail2@yahoo.de>
2020-07-11 21:12:34 +0200

    fixed player jumping behaviour and added gravity

--- 3D Game Jam/Assets/Scripts/ThirdPersonMovement.cs
+++ 3D Game Jam/Assets/Scripts/ThirdPersonMovement.cs
@@ -1,44 +1,69 @@
-﻿using System.Runtime.Remoting.Messaging;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
     public Transform Camera;
     public CharacterController Controller;
 
+    public float    playerSpeed = 25f,
+                    jumpHeight = 4f,
+                    jumpPower = 2f,
+                    gravityMultiplier = 0.15f;
+
     private Vector3 playerVelocity = new Vector3(0, 0, 0);
-    private bool isGrounded = false;
-    private float playerSpeed = 20.0f;
-    private float jumpHeight = 1.5f;
-    private float gravityValue = -9.81f;
-    //private float previousTransformY;
+    private bool isGrounded, canJump;
+    private float previousTransformY;
+
+    private void Start()
+    {
+        previousTransformY = -1000f;
+        canJump = isGrounded = Controller.isGrounded;
+    }
 
     private void FixedUpdate()
     {
         isGrounded = Controller.isGrounded;
-        if (isGrounded && playerVelocity.y < 0)
+
+        RotateCharacterTowardsCameraAim();
+
+        MoveCharacterTowardsRotation();
+
+        ExecuteIfJumped();
+
+        AddGravityToCharacter();
+    }
+
+    private void RotateCharacterTowardsCameraAim() => transform.rotation = Quaternion.Euler(0f, Camera.transform.rotation.eulerAngles.y, 0f);
+
+    private void MoveCharacterTowardsRotation() => Controller.Move(transform.forward * playerSpeed * Time.fixedDeltaTime);
+
+    private void ExecuteIfJumped()
+    {
+        if(!canJump) return;
+
+        if(Input.GetKey(KeyCode.Space))
         {
-            playerVelocity.y = 0f;
+            playerVelocity.y += Mathf.Pow(jumpHeight, jumpPower);
+            Controller.Move(playerVelocity * Time.fixedDeltaTime);
+            canJump = false;
+        }
+    }
+
+    private void AddGravityToCharacter()
+    {
+        if (isGrounded)
+        {
+            playerVelocity = Vector3.zero;
+            previousTransformY = transform.position.y;
+            canJump = true;
+            return;
         }
 
-        transform.rotation = Quaternion.Euler(0f, Camera.transform.rotation.eulerAngles.y, 0f);
-        
-        var move = transform.forward;
-        Controller.Move(move * playerSpeed * Time.fixedDeltaTime);
-
-        //if (move != Vector3.zero)
-        //{
-        //    gameObject.transform.forward = move;
-        //}
-
-        // Changes the height position of the player..
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (previousTransformY != transform.position.y)
         {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            playerVelocity.y += Physics.gravity.y * gravityMultiplier;
+            Controller.Move(playerVelocity * Time.fixedDeltaTime);
         }
-
-        playerVelocity.y += gravityValue * Time.fixedDeltaTime;
-        Controller.Move(playerVelocity * Time.fixedDeltaTime);
     }
 
 }

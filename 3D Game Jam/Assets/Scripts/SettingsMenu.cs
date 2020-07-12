679a1dd515d03aa38c9bb23cd93fbb736a0ca7bb
Finn <zockeremail2@yahoo.de>
2020-07-12 19:34:31 +0200

    added start and settings menu

---
+++ 3D Game Jam/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public string exposedParameterName;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat(exposedParameterName, volume);
+    }
+}

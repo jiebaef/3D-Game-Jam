6b4d215f50f2a755d13b5f4bab88a5d3ad80ef1b
Finn <zockeremail2@yahoo.de>
2020-07-12 04:33:16 +0200

    fixed merge conflicts in ThirdPersonMovement script

--- 3D Game Jam/Assets/Scripts/ThirdPersonMovement.cs
+++ 3D Game Jam/Assets/Scripts/ThirdPersonMovement.cs
@@ -3,7 +3,6 @@
 public class ThirdPersonMovement : MonoBehaviour
 {
     public Transform Camera;
-    public Transform groundCheck;
     public CharacterController Controller;
 
     public float playerSpeed = 25f,
@@ -12,7 +11,6 @@ public class ThirdPersonMovement : MonoBehaviour
                     gravityMultiplier = 0.15f;
 
     private Vector3 playerVelocity = new Vector3(0, 0, 0);
-<<<<<<< HEAD
     private bool isGrounded, canJump;
 
     private void Start()
@@ -23,25 +21,6 @@ public class ThirdPersonMovement : MonoBehaviour
     private void FixedUpdate()
     {
         isGrounded = Controller.isGrounded;
-=======
-    private bool isGrounded = false;
-    private float playerSpeed = 20.0f;
-    private float jumpHeight = 1f;
-    private float gravityValue = -9.81f;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-
-
-    private void FixedUpdate()
-    {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        Debug.Log(isGrounded);
-
-        if (isGrounded && playerVelocity.y < 0)
-        {
-            playerVelocity.y = -2f;
-        }
->>>>>>> origin/Tim2
 
         RotateCharacterTowardsCameraAim();
 
@@ -54,7 +33,6 @@ public class ThirdPersonMovement : MonoBehaviour
 
     private void RotateCharacterTowardsCameraAim() => transform.rotation = Quaternion.Euler(0f, Camera.transform.rotation.eulerAngles.y, 0f);
 
-<<<<<<< HEAD
     private void MoveCharacterTowardsRotation() => Controller.Move(transform.forward * playerSpeed * Time.fixedDeltaTime);
 
     private void ExecuteIfJumped()
@@ -72,9 +50,6 @@ public class ThirdPersonMovement : MonoBehaviour
     private void AddGravityToCharacter()
     {
         if (isGrounded)
-=======
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
->>>>>>> origin/Tim2
         {
             playerVelocity = Vector3.zero;
             canJump = true;
